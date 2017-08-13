31219c5e7071951ef5ceaf2aaa83a2b9257ce90f
William Stevens <contact@wastevensv.com>
2017-08-12 20:07:51 -0400

    Changed theme.

--- init.el
+++ init.el
@@ -162,6 +162,7 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (tsdh-dark)))
  '(inhibit-startup-screen t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
